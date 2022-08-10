d679523b85fdd5f2a3e4eadf119126532d054437
luanps2 <luan-esposito@hotmail.com>
2022-08-10 00:50:32 -0300

    Ajustes no Layout

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -55,6 +55,8 @@
             this.pbQR1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbQR20)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbQR15)).BeginInit();
@@ -83,9 +85,9 @@
             this.btnGerar.BackColor = System.Drawing.Color.Green;
             this.btnGerar.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnGerar.ForeColor = System.Drawing.Color.White;
-            this.btnGerar.Location = new System.Drawing.Point(12, 426);
+            this.btnGerar.Location = new System.Drawing.Point(6, 457);
             this.btnGerar.Name = "btnGerar";
-            this.btnGerar.Size = new System.Drawing.Size(128, 88);
+            this.btnGerar.Size = new System.Drawing.Size(128, 53);
             this.btnGerar.TabIndex = 0;
             this.btnGerar.Text = "Gerar";
             this.btnGerar.UseVisualStyleBackColor = false;
@@ -93,10 +95,10 @@
             // 
             // txtDados
             // 
-            this.txtDados.Location = new System.Drawing.Point(12, 68);
+            this.txtDados.Location = new System.Drawing.Point(12, 86);
             this.txtDados.Multiline = true;
             this.txtDados.Name = "txtDados";
-            this.txtDados.Size = new System.Drawing.Size(255, 313);
+            this.txtDados.Size = new System.Drawing.Size(249, 295);
             this.txtDados.TabIndex = 1;
             // 
             // comboBox1
@@ -115,9 +117,9 @@
             "Promotor de Vendas",
             "Teatro",
             "CJ"});
-            this.comboBox1.Location = new System.Drawing.Point(12, 387);
+            this.comboBox1.Location = new System.Drawing.Point(12, 416);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(255, 23);
+            this.comboBox1.Size = new System.Drawing.Size(249, 23);
             this.comboBox1.TabIndex = 3;
             // 
             // label1
@@ -322,9 +324,9 @@
             this.button1.BackColor = System.Drawing.Color.DarkRed;
             this.button1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(146, 426);
+            this.button1.Location = new System.Drawing.Point(140, 457);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(121, 88);
+            this.button1.Size = new System.Drawing.Size(121, 53);
             this.button1.TabIndex = 6;
             this.button1.Text = "Limpar";
             this.button1.UseVisualStyleBackColor = false;
@@ -335,18 +337,40 @@
             this.button2.BackColor = System.Drawing.Color.MidnightBlue;
             this.button2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(12, 530);
+            this.button2.Location = new System.Drawing.Point(6, 517);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(255, 75);
+            this.button2.Size = new System.Drawing.Size(255, 56);
             this.button2.TabIndex = 7;
             this.button2.Text = "Salvar Imagens";
             this.button2.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 21);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Insira os nomes:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 392);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 21);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Selecione o curso:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1133, 629);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
@@ -412,5 +436,7 @@
         private PictureBox pbQR1;
         private Button button1;
         private Button button2;
+        private Label label2;
+        private Label label3;
     }
 }
